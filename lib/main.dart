878e968ed4a09e2f22b25f858fc918e6eaf707e6
Imtiaz1245 <imtiaz12685@gmail.com>
2022-03-15 18:11:08 +0500

    message

--- lib/main.dart
+++ lib/main.dart
@@ -14,36 +14,37 @@ class _MyAppState extends State<MyApp> {
   @override
 
   Widget build(BuildContext context) {
- List<String> names=["achar","burger","chili","salt","samosa","sandwich","suger"];
-List<String> images=["achar.png","burger.jpg","chili.png","salt.png","samosa.jpg","sandwich.jpg","suger.jpg"];
+    List<String> names=["achar","burger","chili","salt","samosa","sandwich","suger"];
+    List<String> images=["achar.png","burger.jpg","chili.png","salt.png","samosa.jpg","sandwich.jpg","suger.jpg"];
     return
-       Scaffold(
+      Scaffold(
         appBar: AppBar(
           title: Text("new app"),
-        centerTitle: true,
-        titleSpacing: 4.0,),
+          centerTitle: true,
+          titleSpacing: 4.0,),
         body: Center(
           child: Container(
             alignment: Alignment.center,
-            width: 150,
+            width: 200,
             height: 150,
-            child: Text("I am Imtiaz  fkxjzcgjh"),
-            padding: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: Colors.red,
-              border: Border.all(width: 10,color: Colors.black,style: BorderStyle.solid),
-           boxShadow: [BoxShadow(
-                color: Colors.blue,
-             blurRadius: 10,
-             spreadRadius: 7
-            )]
+            child: Text.rich(
+              TextSpan(
+                text: 'HI',
+                style: TextStyle(color: Colors.blue),// default text style
+                children: [
+                  TextSpan(text: ' Imtiaz ', style: TextStyle(fontStyle: FontStyle.italic,fontSize: 20,color: Colors.white)),
+                ],
+              ),
             ),
+            decoration: BoxDecoration(
+              color: Colors.orangeAccent,
+              border: Border.all(width: 10,color: Colors.black,style: BorderStyle.solid),
 
+
+            ),
           ),
         ),
-
-
-    );
+      );
   }
 
   Widget contai(int index) {
